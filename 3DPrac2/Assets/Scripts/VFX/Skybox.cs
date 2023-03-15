d20aa45500cdcfebafcb9331432ebc39da0a0e68
kevinwang1011 <84383667+kevinwang1011@users.noreply.github.com>
2023-03-15 23:56:42 +0800

    Rewrote nighttime skybox / ambient

--- 3DPrac2/Assets/Scripts/VFX/Skybox.cs
+++ 3DPrac2/Assets/Scripts/VFX/Skybox.cs
@@ -6,7 +6,7 @@ public class Skybox : MonoBehaviour
 {
 
     [SerializeField] private Material skyboxMaterial;
-    
+
     [SerializeField] private Shader skyboxShader;
     [Range(1, 10)] public int timePerTick = 5;
     private Renderer _renderer;
@@ -17,17 +17,20 @@ public class Skybox : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
         ticktime = 0;
         MaxTickTime = timePerTick * 720;
         skyboxShader = Shader.Find("Skybox/Cubemap Blend");
-        if(skyboxShader == null) {
+        if (skyboxShader == null)
+        {
             Debug.Log("Skybox Shader not found");
         }
-        else {
+        else
+        {
             Debug.Log("Skybox Shader found");
         }
-        if(skyboxMaterial == null) {
+        if (skyboxMaterial == null)
+        {
             Debug.Log("Skybox Material not found");
             skyboxMaterial = new Material(skyboxShader);
             RenderSettings.skybox = skyboxMaterial;
@@ -37,51 +40,80 @@ public class Skybox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         ticktime++;
         MaxTickTime = timePerTick * 720;
-        if (ticktime > MaxTickTime) {
+        if (ticktime > MaxTickTime)
+        {
             ticktime = 0;
         }
-        if(ticktime%timePerTick==0) UpdateMaterialVariables(ticktime / timePerTick);
+        if (ticktime % timePerTick == 0) UpdateMaterialVariables(ticktime / timePerTick);
     }
 
-    void UpdateMaterialVariables(int tt) {
+    void UpdateMaterialVariables(int tt)
+    {
         //daytime = 0 ~ 360
-            //sunrise = 0 ~ 90 
-                //Trasiition 1 ~ 0.8
-            if(tt <= 90) {
-                skyboxMaterial.SetFloat("_CubemapTransition", 1.0f - (tt / 450f));
-            }
-            //day = 90 ~ 270
-                //Trasiition 0.8 ~ 0 ~ 0.8
-            else if(tt <= 270) {
-                skyboxMaterial.SetFloat("_CubemapTransition", (Mathf.Abs((tt - 180f) / 112.5f)));
-            }
-            //sunset = 270 ~ 360
-                //Trasiition 0.8 ~ 1
-            else if(tt <= 360) {
-                skyboxMaterial.SetFloat("_CubemapTransition", 0.2f + (tt / 450f));
-            }
+
+        //sunrise = 0 ~ 90 
+        //Trasiition 1 ~ 0.8
+        if (tt <= 90)
+        {
+            skyboxMaterial.SetFloat("_CubemapTransition", 1.0f - (tt / 450f));
+        }
+        //day = 90 ~ 270
+        //Trasiition 0.8 ~ 0 ~ 0.8
+        else if (tt <= 270)
+        {
+            skyboxMaterial.SetFloat("_CubemapTransition", (Mathf.Abs((tt - 180f) / 112.5f)));
+        }
+        //sunset = 270 ~ 360
+        //Trasiition 0.8 ~ 1
+        else if (tt <= 360)
+        {
+            skyboxMaterial.SetFloat("_CubemapTransition", 0.2f + (tt / 450f));
+        }
+
+
         //nighttime = 360 ~ 720
-            //moonrise = 360 ~ 450
-                //Exposure 0.9 ~ 0.45
-            else if(tt <= 450) {
-                skyboxMaterial.SetFloat("_Exposure", 2.7f - (tt / 200f));
+
+        //moonrise = 360 ~ 450
+        //Exposure 0.9 ~ 0
+        else if (tt <= 450)
+        {
+            skyboxMaterial.SetFloat("_Exposure", 4.5f - (tt / 100f));
+        }
+        //night = 450 ~ 630
+        //Exposure 0
+        else if (tt <= 630)
+        {
+            //skyboxMaterial.SetFloat("_Exposure", (Mathf.Abs((tt - 540f) / 150f)));
+
+        }
+        //moonset = 630 ~ 720
+        //Exposure 0.3 ~ 0
+        else if (tt <= 720)
+        {
+            skyboxMaterial.SetFloat("_Exposure", ((tt - 630f) / 100f));
+            
+        }
+
+        //Ambient Light Update
+        if (tt <= 540)
+        {
+            RenderSettings.ambientIntensity = 3.6f - (Mathf.Abs(tt - 180f) / 100f);
+            if(tt==540)
+            {
+                Debug.Log("Midnight");
             }
-            //night = 450 ~ 630
-                //Exposure 0.45 ~ 0 ~ 0.45
-            else if(tt <= 630) {
-                skyboxMaterial.SetFloat("_Exposure", (Mathf.Abs((tt - 540f) / 200f)));
-            }
-            //moonset = 630 ~ 720
-                //Exposure 0.45 ~ 0.9
-            else if(tt <= 720) {
-                skyboxMaterial.SetFloat("_Exposure", 0.45f + ((tt - 630f) / 200f));
-            }
+        }
+        else
+        {
+            RenderSettings.ambientIntensity = (tt - 540f) / 100f;
+        }
     }
 
 }
